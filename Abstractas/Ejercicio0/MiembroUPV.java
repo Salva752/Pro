6a2ad0ee63e786d233586b5e1a623f005e90473c
Salva752 <60731162+Salva752@users.noreply.github.com>
2020-04-26 18:10:44 +0200

    Add files via upload

---
+++ Abstractas/Ejercicio0/MiembroUPV.java
@@ -0,0 +1,16 @@
+//Salva Juan Soria
+package Abstractas.Ejercicio0;
+
+public abstract class MiembroUPV extends Persona{
+	String carnet;
+
+		public String getCarnet() {
+			return carnet;
+		}
+
+		public void setCarnet(String carnet) {
+			this.carnet = carnet;
+		}
+		
+		public abstract void menuIntranet();
+}
